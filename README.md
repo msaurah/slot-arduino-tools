7654c2d2d986202680bea0545fb06a6a7d026926
msaurah <msaurah@gmail.com>
2023-01-05 14:27:08 +0100

    chrono with button control

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Tools to improve the Scalextric experience with Arduino
 
 # refs
 https://docs.arduino.cc/learn/electronics/lcd-displays
+https://www.hackster.io/shashwatraj98765/how-to-make-stopwatch-with-arduino-6e09db
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <Bounce2.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
@@ -8,19 +9,59 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+signed short minutes, secondes;
+char timeline[16];
+
+// define pins for buttons
+const int startPin = 6;
+Bounce startDebouncer = Bounce();
+
+bool startState = LOW;
+
+unsigned long startMillis;
+unsigned long currentMillis;
+unsigned long elapsedMillis;
+
 void setup() {
+  //define INPUT pins
+  pinMode(startPin, INPUT);
+  startDebouncer.attach(startPin);
+  startDebouncer.interval(5); 
+  // clean the display
+  lcd.clear();
   // put your setup code here, to run once:
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+  lcd.print("Cronometro:");
+  lcd.setCursor(0,1);
+  lcd.print("Pulsa START");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  // Update the Bounce instances :
+  startDebouncer.update();
+
+  if ( startDebouncer.fell() ) {     // Call code if button transitions from HIGH to LOW
+    startState = HIGH;         // Toggle start button state
+    startMillis = millis();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Cronometro:");
+  }
+
+  if (startState)
+  {
+    currentMillis = millis();
+    elapsedMillis = (currentMillis - startMillis);
+    
+    unsigned long durMS = (elapsedMillis%1000);       //Milliseconds
+    unsigned long durSS = (elapsedMillis/1000)%60;    //Seconds
+    unsigned long durMM = (elapsedMillis/(60000))%60; //Minutes
+ 
+    //String durMilliSec = timeMillis(durHH, durMM, durSS,durMS);
+    sprintf(timeline,"%0.2lu:%0.2lu:%0.2lu", durMM, durSS, durMS);
+    lcd.setCursor(0, 1);
+    lcd.print(timeline);
+  }
 }
